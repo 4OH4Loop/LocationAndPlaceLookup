842b13fb28f575c6ddc84c417f7c799d1c3795bd
4OH4Loop <semperchuck@gmail.com>
2025-04-06 18:36:09 -0400

    Initial Commit

---
+++ LocationAndPlaceLookup/LocationAndPlaceLookupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAndPlaceLookupApp.swift
+//  LocationAndPlaceLookup
+//
+//  Created by Carolyn Ballinger on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct LocationAndPlaceLookupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
